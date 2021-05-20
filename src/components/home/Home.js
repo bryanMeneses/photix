ae3ff75572865de01fee0be3da116a7a09ec1b4e
Bryan Meneses <bryanmeneses@Bryans-MacBook-Pro.local>
2021-05-20 16:45:34 +0100

    updating api url in Latest page and removing console logging

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,114 +1,138 @@
-import React, { Component } from 'react'
-import { Carousel, Spinner } from 'react-bootstrap'
-import './Home.css'
-import { createApi } from 'unsplash-js';
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Carousel, Spinner } from "react-bootstrap";
+import "./Home.css";
+import { createApi } from "unsplash-js";
+import { Link } from "react-router-dom";
 
 const unsplash = createApi({
-    apiUrl: "https://photix-server.netlify.app/.netlify/functions/index/unsplash"
-})
+  apiUrl: "https://photix-server.netlify.app/.netlify/functions/index/unsplash",
+});
 
 class Home extends Component {
-    state = {
-        randomImages: [],
-        requestError: false
-    }
+  state = {
+    randomImages: [],
+    requestError: false,
+  };
 
-    componentDidMount() {
-        unsplash.photos.getRandom({
-            count: 3,
-            query: 'landscape'
-        })
-            .then(result => {
-                console.log(result);
-                if (result.errors) {
-                    console.log(result.errors);
-                    this.setState({ requestError: true })
-                } else {
-                    console.log(result);
-                    this.setState({ randomImages: result.response.response, requestError: false })
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                this.setState({ requestError: true })
-            });
-    }
-    render() {
-        const { randomImages, requestError } = this.state
-        if (requestError) {
-            return (
-                <div>
-                    <div style={{
-                        width: '100%',
-                        margin: 'auto',
-                        position: 'absolute',
-                        top: '30%',
-
-                    }} >
-
-                        <h1 className="display-4">:( Whoops...</h1>
-                        <h3>something went wrong!</h3>
-                        <h5 className="w-75 my-4 mx-auto">There is a limit to how many photos we can request. Please check back in a bit!</h5>
-                    </div>
-                </div>
-            )
+  componentDidMount() {
+    unsplash.photos
+      .getRandom({
+        count: 3,
+        query: "landscape",
+      })
+      .then((result) => {
+        if (result.errors) {
+          // console.log(result.errors);
+          this.setState({ requestError: true });
+        } else {
+          this.setState({
+            randomImages: result.response.response,
+            requestError: false,
+          });
         }
-        if (!randomImages[0]) {
-            return (
-                <Spinner
-                    style={{
-                        position: "absolute",
-                        top: '50%',
-                        bottom: '50%',
-                        left: '50%',
-                        right: '50%',
-                    }}
-                    animation="border"
-                    role="status">
-                    <span className="sr-only">Loading...</span>
-                </Spinner>
-            )
-        }
-        return (
-            <React.Fragment >
-                <div className='animated fadeIn'>
-
-                    <div className="center text-white">
-                        <h1 className="display-4">Need new ideas?</h1>
-                        <p className="Home__small">Take a look through our collections and start seeing a new perspective.</p>
-                        <Link to="/latest">
-                            <button className="myBtn myBtn-4 myBtn-4c icon-arrow-right">
-                                See More
-                            </button>
-                        </Link>
-                    </div>
-
-                    <Carousel
-                        controls={false}
-                        indicators={false}
-                        pauseOnHover={false}
-                        touch={false}
-                    >
-                        {randomImages.map(image => {
-                            return (
-                                <Carousel.Item
-                                    key={image.id}
-                                    className='fixedBackground'
-                                    style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(${image.urls.regular})` }}>
-                                    <div className="carousel-item-style">
-                                        <p>Taken by {image.user.name}</p>
-                                        {image.user.portfolio_url ? (<p><a style={{ color: 'white' }} target='blank' href={image.user.portfolio_url}>See Profile</a></p>) : null}
-                                        {image.location.title !== null ? (`Taken in ${image.location.title}`) : (<i>Taken somewhere on earth</i>)}
-                                    </div>
-                                </Carousel.Item>
-                            )
-                        })}
-                    </Carousel>
-                </div>
-            </React.Fragment >
-        )
+      })
+      .catch((err) => {
+        // console.log(err);
+        this.setState({ requestError: true });
+      });
+  }
+  render() {
+    const { randomImages, requestError } = this.state;
+    if (requestError) {
+      return (
+        <div>
+          <div
+            style={{
+              width: "100%",
+              margin: "auto",
+              position: "absolute",
+              top: "30%",
+            }}
+          >
+            <h1 className="display-4">:( Whoops...</h1>
+            <h3>something went wrong!</h3>
+            <h5 className="w-75 my-4 mx-auto">
+              There is a limit to how many photos we can request. Please check
+              back in a bit!
+            </h5>
+          </div>
+        </div>
+      );
     }
+    if (!randomImages[0]) {
+      return (
+        <Spinner
+          style={{
+            position: "absolute",
+            top: "50%",
+            bottom: "50%",
+            left: "50%",
+            right: "50%",
+          }}
+          animation="border"
+          role="status"
+        >
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+    }
+    return (
+      <React.Fragment>
+        <div className="animated fadeIn">
+          <div className="center text-white">
+            <h1 className="display-4">Need new ideas?</h1>
+            <p className="Home__small">
+              Take a look through our collections and start seeing a new
+              perspective.
+            </p>
+            <Link to="/latest">
+              <button className="myBtn myBtn-4 myBtn-4c icon-arrow-right">
+                See More
+              </button>
+            </Link>
+          </div>
+
+          <Carousel
+            controls={false}
+            indicators={false}
+            pauseOnHover={false}
+            touch={false}
+          >
+            {randomImages.map((image) => {
+              return (
+                <Carousel.Item
+                  key={image.id}
+                  className="fixedBackground"
+                  style={{
+                    backgroundImage: `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(${image.urls.regular})`,
+                  }}
+                >
+                  <div className="carousel-item-style">
+                    <p>Taken by {image.user.name}</p>
+                    {image.user.portfolio_url ? (
+                      <p>
+                        <a
+                          style={{ color: "white" }}
+                          target="blank"
+                          href={image.user.portfolio_url}
+                        >
+                          See Profile
+                        </a>
+                      </p>
+                    ) : null}
+                    {image.location.title !== null ? (
+                      `Taken in ${image.location.title}`
+                    ) : (
+                      <i>Taken somewhere on earth</i>
+                    )}
+                  </div>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-export default Home
+export default Home;
--- src/components/images/Latest.js
+++ src/components/images/Latest.js
@@ -1,197 +1,230 @@
-import React, { Component } from 'react'
-import { Spinner, Form, FormControl, Button } from 'react-bootstrap'
-import Image from './Image';
+import React, { Component } from "react";
+import { Spinner, Form, FormControl, Button } from "react-bootstrap";
+import Image from "./Image";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 class Latest extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            collections: [],
-            isLoaded: false,
-            images: [],
-            page: 1,
-            perPage: 15,
-            searchfield: ''
-        }
-    }
-    componentDidMount() {
-        const { page, perPage } = this.state
-
-        // This call fills the main gallery's first 15 pictures
-        // Don't know why Unsplash browser API isn't working but this call to our own api works just fine.
-        fetch(`https://photix-server.netlify.app/.netlify/functions/index/unsplash/photos?per_page=${perPage}&page=${page}`)
-            .then(res => res.json())
-            .then(data => {
-                if (data.errors) {
-                    console.log(data.erorrs);
-                } else {
-                    this.setState({
-                        images: data.response.results,
-                        page: page + 1,
-                    })
-                }
-            })
-
-        // unsplash.photos.list({
-        //     page: 1,
-        //     perPage: 15,
-        // })
-        //     .then(result => {
-        //         console.log(result);
-        //         // if (result.errors) {
-        //             // console.log(result.errors);
-        //         // } else {
-        //             console.log(result);
-        //             this.setState({ images: result.response.results })
-        //         // }
-        //     })
-        //     .catch(err => console.log(err))
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: [],
+      isLoaded: false,
+      images: [],
+      page: 1,
+      perPage: 15,
+      searchfield: "",
     };
+  }
+  componentDidMount() {
+    const { page, perPage } = this.state;
 
-    onSearchSubmit = e => {
-        e.preventDefault()
-        const { searchfield } = this.state
-
-        window.location.href = `/search?query=${searchfield}`
-    }
-
-    onSearchChange = e => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-
-    fetchMoreImages = () => {
-        const { page, perPage, images } = this.state
-        fetch(`http://localhost:5000/unsplash/photos?per_page=${perPage}&page=${page}`)
-            .then(res => res.json())
-            .then(data => {
-                if (data.errors) {
-                    console.log(data.erorrs);
-                } else {
-                    this.setState({ images: images.concat(data.response.results) })
-                }
-            })
-
-        this.setState({ page: page + 1 })
-    }
-
-    imagesLoaded = parentNode => {
-        const imgElements = parentNode.querySelectorAll("img");
-
-        for (const img of imgElements) {
-            if (img.complete) {
-                return true;
-            }
+    // This call fills the main gallery's first 15 pictures
+    // Don't know why Unsplash browser API isn't working but this call to our own api works just fine.
+    fetch(
+      `https://photix-server.netlify.app/.netlify/functions/index/unsplash/photos?per_page=${perPage}&page=${page}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.errors) {
+          console.log(data.erorrs);
+        } else {
+          this.setState({
+            images: data.response.results,
+            page: page + 1,
+          });
         }
-        return false;
-    }
+      });
 
-    handleStateChange = () => {
-        this.setState({
-            isLoaded: this.imagesLoaded(this.galleryElement),
-        });
-    }
+    // unsplash.photos.list({
+    //     page: 1,
+    //     perPage: 15,
+    // })
+    //     .then(result => {
+    //         console.log(result);
+    //         // if (result.errors) {
+    //             // console.log(result.errors);
+    //         // } else {
+    //             console.log(result);
+    //             this.setState({ images: result.response.results })
+    //         // }
+    //     })
+    //     .catch(err => console.log(err))
+  }
 
-    renderLoadingHeader = () => {
-        if (this.state.isLoaded) {
-            // Render nothing if not loading
-            return null;
+  onSearchSubmit = (e) => {
+    e.preventDefault();
+    const { searchfield } = this.state;
+
+    window.location.href = `/search?query=${searchfield}`;
+  };
+
+  onSearchChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  fetchMoreImages = () => {
+    const { page, perPage, images } = this.state;
+    fetch(
+      `https://photix-server.netlify.app/.netlify/functions/index/unsplash/photos?per_page=${perPage}&page=${page}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.errors) {
+          console.log(data.erorrs);
+        } else {
+          this.setState({ images: images.concat(data.response.results) });
         }
-        return (
-            <Spinner
-                style={{
-                    position: "absolute",
-                    top: '50%',
-                    bottom: '50%',
-                    left: '50%',
-                    right: '50%',
-                }}
-                animation="border"
-                role="status">
-                <span className="sr-only">Loading...</span>
-            </Spinner>
-        )
+      });
+
+    this.setState({ page: page + 1 });
+  };
+
+  imagesLoaded = (parentNode) => {
+    const imgElements = parentNode.querySelectorAll("img");
+
+    for (const img of imgElements) {
+      if (img.complete) {
+        return true;
+      }
     }
+    return false;
+  };
 
-    render() {
-        const { images } = this.state
-        return (
-            <div>
-                {(!images[0]) ? null : (
+  handleStateChange = () => {
+    this.setState({
+      isLoaded: this.imagesLoaded(this.galleryElement),
+    });
+  };
 
-                    <div className="fixedBGLatest" style={{
-                        backgroundImage: `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(https://images.unsplash.com/photo-1620408991409-37585ff089a1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw3MTkwMnwwfDF8cmFuZG9tfHx8fHx8fHx8MTYyMTE2NTMxNw&ixlib=rb-1.2.1&q=80&w=1080)`
-                    }}>
-                        <div className="text-left text-white" style={{ width: '80%', margin: "0px auto", paddingTop: '200px' }}>
-                            <h1 className="display-4" style={{ fontWeight: '400' }} >Our Latest Collection</h1>
-                            <h5>Browse high definition images uploaded by users like you</h5>
+  renderLoadingHeader = () => {
+    if (this.state.isLoaded) {
+      // Render nothing if not loading
+      return null;
+    }
+    return (
+      <Spinner
+        style={{
+          position: "absolute",
+          top: "50%",
+          bottom: "50%",
+          left: "50%",
+          right: "50%",
+        }}
+        animation="border"
+        role="status"
+      >
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  };
 
-                            <Form onSubmit={this.onSearchSubmit} inline>
-                                <FormControl
-                                    style={{ width: '60%' }}
-                                    required
-                                    value={this.state.searchfield}
-                                    name="searchfield"
-                                    onChange={this.onSearchChange}
-                                    type="text"
-                                    placeholder="Search"
-                                    className="mr-sm-2 mr-2 my-3" />
-                                <Button type="submit" variant="light">Search</Button>
-                            </Form>
-                            <p>Try something like {' '}
-                                <span
-                                    className="text-link"
-                                    onClick={() => window.location.href = `/search?query=black and white`}>
-                                    "black and white"
-                                </span>, {' '}
-                                <span
-                                    className="text-link"
-                                    onClick={() => window.location.href = `/search?query=nature`}>
-                                    "nature"
-                                </span>, or {' '}
-                                <span
-                                    className="text-link"
-                                    onClick={() => window.location.href = `/search?query=flower`}>
-                                    "flower"
-                                </span>.
-                            </p>
-                        </div>
+  render() {
+    const { images } = this.state;
+    return (
+      <div>
+        {!images[0] ? null : (
+          <div
+            className="fixedBGLatest"
+            style={{
+              backgroundImage: `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(https://images.unsplash.com/photo-1620408991409-37585ff089a1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw3MTkwMnwwfDF8cmFuZG9tfHx8fHx8fHx8MTYyMTE2NTMxNw&ixlib=rb-1.2.1&q=80&w=1080)`,
+            }}
+          >
+            <div
+              className="text-left text-white"
+              style={{ width: "80%", margin: "0px auto", paddingTop: "200px" }}
+            >
+              <h1 className="display-4" style={{ fontWeight: "400" }}>
+                Our Latest Collection
+              </h1>
+              <h5>Browse high definition images uploaded by users like you</h5>
 
-
-                    </div>
-                )}
-
-                <div style={{ width: '90%', margin: '20px auto' }} className="gallery" ref={element => { this.galleryElement = element; }}>
-                    <InfiniteScroll
-                        dataLength={this.state.images.length}
-                        next={this.fetchMoreImages}
-                        hasMore={true}
-                        loader={<Spinner animation="border" role="status"> <span className="sr-only">Loading...</span>
-                        </Spinner>}
-                    >
-                        {this.renderLoadingHeader()}
-                        <div className="images" >
-                            {images.map((image, i) => {
-                                return (
-                                    <Image
-                                        key={i}
-                                        image={image}
-                                        id={image.id}
-                                        src={image.urls.small}
-                                        alt={image.description}
-                                        handleStateChange={this.handleStateChange}
-                                    />
-                                )
-                            })}
-                        </div>
-                    </InfiniteScroll>
-                </div>
+              <Form onSubmit={this.onSearchSubmit} inline>
+                <FormControl
+                  style={{ width: "60%" }}
+                  required
+                  value={this.state.searchfield}
+                  name="searchfield"
+                  onChange={this.onSearchChange}
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2 mr-2 my-3"
+                />
+                <Button type="submit" variant="light">
+                  Search
+                </Button>
+              </Form>
+              <p>
+                Try something like{" "}
+                <span
+                  className="text-link"
+                  onClick={() =>
+                    (window.location.href = `/search?query=black and white`)
+                  }
+                >
+                  "black and white"
+                </span>
+                ,{" "}
+                <span
+                  className="text-link"
+                  onClick={() =>
+                    (window.location.href = `/search?query=nature`)
+                  }
+                >
+                  "nature"
+                </span>
+                , or{" "}
+                <span
+                  className="text-link"
+                  onClick={() =>
+                    (window.location.href = `/search?query=flower`)
+                  }
+                >
+                  "flower"
+                </span>
+                .
+              </p>
             </div>
-        )
-    }
+          </div>
+        )}
+
+        <div
+          style={{ width: "90%", margin: "20px auto" }}
+          className="gallery"
+          ref={(element) => {
+            this.galleryElement = element;
+          }}
+        >
+          <InfiniteScroll
+            dataLength={this.state.images.length}
+            next={this.fetchMoreImages}
+            hasMore={true}
+            loader={
+              <Spinner animation="border" role="status">
+                {" "}
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            }
+          >
+            {this.renderLoadingHeader()}
+            <div className="images">
+              {images.map((image, i) => {
+                return (
+                  <Image
+                    key={i}
+                    image={image}
+                    id={image.id}
+                    src={image.urls.small}
+                    alt={image.description}
+                    handleStateChange={this.handleStateChange}
+                  />
+                );
+              })}
+            </div>
+          </InfiniteScroll>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-export default Latest
+export default Latest;
